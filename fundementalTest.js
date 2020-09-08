59a8473951e0155ade254b27ea01adc1e3ca888e
Kishore Newton <kishorenke@gmail.com>
2020-09-08 19:09:17 +0530

    seperated files

--- fundementalTest.js
+++ fundementalTest.js
@@ -6,21 +6,8 @@ function sumTest() {
     let expected = 3
     expect(result).toBe(expected)
 }
+
 test('sum adds numbers', sumTest)
-/**
- * simple way
- * 
- */
-
-// let result = sum(3, 7)
-// let expected = 10 
-// if(result !== expected) throw new Error(`${result} is not equal to ${expected}`)
-//     else console.log('working')
-
-// result = subtract(10, 5)
-// expected = 5
-// if(result !== expected) throw new Error(`${result} is not equal to ${expected}`)
-//     else console.log('working')
 
 
 /**
